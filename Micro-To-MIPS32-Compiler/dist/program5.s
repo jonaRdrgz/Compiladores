ec48705a5554bc9ac7aaa08a6468ad961ef5913f
jonaRdrgz <jonastu.9@gmail.com>
2017-02-24 13:51:47 -0600

    actualizacion

--- Micro-To-MIPS32-Compiler/dist/program5.s
+++ Micro-To-MIPS32-Compiler/dist/program5.s
@@ -15,34 +15,44 @@ ldr r0, addr_var2_num
 ldr r0,[r0]
 ldr r1,addr_var2
 str r0,[r1]
-ldr  r4, addr_var2
-ldr r4, [r4]
-mov r5, #0
-cmp r4, r5
+ldr r5, addr_var2
+ldr r5, [r5]
+mov r6, #10
+add r4, r5, r6
+ldr r7, addr_var3
+str r4, [r7]
+mov r8, #0
+dldl r9, var3
+cmp r9, r8
 bne label0
-ldr r7, addr_var1
-ldr r7, [r7]
-mov r8, #5
-add r6, r7, r8
-mov r4, r6
-mov r9, #0
-cmp r4, r9
+ldr r1, addr_var1
+ldr r1, [r1]
+mov r2, #5
+add r0, r1, r2
+ldr r3, addr_var3
+str r0, [r3]
+mov r4, #0
+dldl r5, var3
+cmp r5, r4
 bne label2
-mov  r4, #4
+li r6, 4
+sw r6, var3
 b label3
 label2:
-mov  r4, #5
+li r7, 5
+sw r7, var3
 label3:
-mov r4, r4
+ldr  r8, addr_var3
+ldr r8, [r8]
 b label1
 label0:
-mov  r4, #10
+mov  r8, #10
 label1:
-ldr r0, addr_var3
-str r4, [r0]
-ldr  r2, addr_var3
-ldr r2, [r2]
-mov r1, r2
+ldr r9, addr_var3
+str r8, [r9]
+ldr  r1, addr_var3
+ldr r1, [r1]
+mov r1, r1
 ldr r0, =format
 bl printf
 pop {r4,lr}
