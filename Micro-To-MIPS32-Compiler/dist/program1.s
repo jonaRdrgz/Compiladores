489a37f1e511499ea7dce1fc32db48200937e113
jonaRdrgz <jonastu.9@gmail.com>
2017-02-26 15:09:26 -0600

    actualizacion

--- Micro-To-MIPS32-Compiler/dist/program1.s
+++ Micro-To-MIPS32-Compiler/dist/program1.s
@@ -2,9 +2,9 @@
 .global main
 main:
 push {r4,lr}
-ldr r1, Addvar2
+ldr r1, addr_var2
 ldr r1, [r1]
-ldr r2, Addvar2
+ldr r2, addr_var2
 ldr r2, [r2]
 add r0, r1, r2
 mov r4, r0
@@ -13,31 +13,31 @@ add r3, r4, r5
 mov r7, r3
 mov r8, #6
 add r6, r7, r8
-ldr r0, Addvar2
-str r6, [r0]
-ldr r2, Addvar2
+ldr r9, addr_var2
+str r6, [r9]
+ldr r1, addr_var2
+ldr r1, [r1]
+ldr r2, addr_var4
 ldr r2, [r2]
-ldr r3, Addvar4
-ldr r3, [r3]
-add r1, r2, r3
-ldr r5, Addvar2
-str r1, [r5]
-ldr  r7, Addvar2
-ldr r7, [r7]
-mov r1, r7
+add r0, r1, r2
+ldr r3, addr_var2
+str r0, [r3]
+ldr  r5, addr_var2
+ldr r5, [r5]
+mov r1, r5
 ldr r0, =format
 bl printf
 pop {r4,lr}
-bx, lr
+bx lr
 
 
-address_of_return: return
-Addvar2: .word var2
-Addvar4: .word var4
+address_of_return: .word return
+addr_var2: .word var2
+addr_var4: .word var4
 
 .data
 return: .word 0
-format: .asciz "%d\n"
+format: .asciz "Se imprimio- %d\n"
 var2: .word 2
 var4: .word 3
 
--- Micro-To-MIPS32-Compiler/dist/program5.s
+++ Micro-To-MIPS32-Compiler/dist/program5.s
@@ -8,7 +8,6 @@ ldr r2, [r2]
 sub r0, r1, r2
 ldr r3, addr_var1
 str r0, [r3]
-
 ldr r0, addr_var2_scan
 ldr r1, addr_var2_num
 bl scanf
@@ -16,19 +15,16 @@ ldr r0, addr_var2_num
 ldr r0,[r0]
 ldr r1,addr_var2
 str r0,[r1]
-
 ldr r5, addr_var2
 ldr r5, [r5]
 mov r6, #10
 add r4, r5, r6
 ldr r7, addr_var3
 str r4, [r7]
-
 mov r8, #0
 ldr r9, addr_var3
 ldr r9, [r9]
 cmp r9, r8
-
 bne label0
 ldr r1, addr_var1
 ldr r1, [r1]
@@ -82,3 +78,4 @@ var2: .word 0
 var2_scan: .asciz "%d"
 var2_num: .word 0
 var3: .word 0
+
